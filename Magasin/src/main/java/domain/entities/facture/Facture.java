026fa4a03f6104c359e7634f3b3a075e96cc959a
John-Paul-DUBECQ <johnpaul.dubecq@hotmail.fr>
2024-05-30 11:05:10 +0200

    🎨 Mise à jour de la facture

--- Magasin/src/main/java/domain/entities/facture/Facture.java
+++ Magasin/src/main/java/domain/entities/facture/Facture.java
@@ -1,52 +1,77 @@
 package domain.entities.facture;
 
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
 
 import domain.entities.article.Article;
-import domain.values.EAN;
+import domain.entities.paiement.PaiementMethod;
 
 public class Facture {
-    private int numero;
-    private HashMap<EAN, Article> articles;
-    private LocalDateTime dateDeCreation;
-    
-    public Facture(int numero) {
-    	this.numero = numero;
-    	dateDeCreation = LocalDateTime.now();
-    	articles = new HashMap<EAN, Article>();
-    }
-    
-    public int getNumero () {
-    	return numero;
-    }
-    
-    public HashMap<EAN, Article> getArticles () {
-    	return articles;
-    }
-    
-    public float totalMoney () {
-    	float money = 0;
-    	
-    	for (Article article : articles.keySet()) {
-    		money += article.getPrice() * articles.get(article);
-    	}
-    	
-    	return money;
-    }
-    
-    public int totalQuantity () {
-    	int quantity = 0;
-    	
-    	for (Integer qte : articles.values()) {
-    		quantity += qte;
-    	}
-    	
-    	return quantity;
-    }
-    
-    public LocalDateTime getDateCreationDateTime () {
-    	return dateDeCreation;
-    }
+	private int numero;
+	private ArrayList<Article> articles;
+	private LocalDateTime creationDateTime;
+	private PaiementMethod moyenPaiement;
+
+	public Facture(int numero) {
+		this.numero = numero;
+		creationDateTime = LocalDateTime.now();
+		articles = new ArrayList<Article>();
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public ArrayList<Article> getArticles() {
+		return articles;
+	}
+
+	public LocalDateTime getDateCreationDateTime() {
+		return creationDateTime;
+	}
+
+	public PaiementMethod getMoyenPaiement() {
+		return moyenPaiement;
+	}
+
+	public void setArticles(ArrayList<Article> articles) {
+		this.articles = articles;
+	}
+
+	public void setCreationDateTime(LocalDateTime date) {
+		if (date == null) {
+			throw new IllegalArgumentException();
+		}
+
+		this.creationDateTime = date;
+	}
+
+	public void setMoyenPaiement(PaiementMethod moyenPaiement) {
+		if (moyenPaiement == null) {
+			throw new IllegalArgumentException();
+		}
+
+		this.moyenPaiement = moyenPaiement;
+	}
+
+	public float totalMoney() {
+		float money = 0;
+
+		for (Article article : articles) {
+			money += article.getPrice() * article.getQuantity();
+		}
+
+		return money;
+	}
+
+	public int totalQuantity() {
+		int quantity = 0;
+
+		for (Article article : articles) {
+			quantity += article.getQuantity();
+		}
+
+		return quantity;
+	}
+
 }
