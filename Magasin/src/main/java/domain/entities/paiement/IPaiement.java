7081a95d98955ff4e34056a430ffeed91f3cb9b1
le-coatFrederic <lecoatfred@gmail.com>
2024-05-30 10:47:27 +0200

    Suppression des fichiers inutiles

--- Magasin/src/main/java/domain/entities/paiement/IPaiement.java
+++
@@ -1,8 +0,0 @@
-package domain.entities.paiement;
-
-public interface IPaiement {
-	public String getIntitule();
-	public PaiementMethod getMethod();
-	public void setIntitule (String intitule);
-	public void setMethod(PaiementMethod method);
-}
--- Magasin/src/main/java/usecase/crudArticle/IArticleDBOutput.java
+++
@@ -1,9 +0,0 @@
-package usecase.crudArticle;
-
-import domain.entities.article.Article;
-
-public interface IArticleDBOutput {
-	void createArticle(Article newArticle);
-	void updateArticle(Article oldArticle, Article newArticle);
-	void removeArticle(Article article);
-}
